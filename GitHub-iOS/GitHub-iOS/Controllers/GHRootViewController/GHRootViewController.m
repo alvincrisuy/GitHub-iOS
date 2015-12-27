61caa74d1bdefc039cd5786d9139a17741bf7e22
Alvin Cris Uy <alvincrisuy@gmail.com>
2015-12-27 19:33:13 +0800

    - Add Search View Controller - Add Repository List View Controller - Add Repository View Controller

---
+++ GitHub-iOS/GitHub-iOS/Controllers/GHRootViewController/GHRootViewController.m
@@ -0,0 +1,37 @@
+//
+//  GHRootViewController.m
+//  GitHub-iOS
+//
+//  Created by Alvin Cris Uy on 27/12/2015.
+//  Copyright © 2015 Alvin Cris Uy. All rights reserved.
+//
+
+#import "GHRootViewController.h"
+
+@interface GHRootViewController ()
+
+@end
+
+@implementation GHRootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
